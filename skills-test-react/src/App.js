d41cea018bf5adcd55004f4fbd968eadc33c59a1
amir-gb <125048149+amir-gb@users.noreply.github.com>
2023-05-03 19:20:14 +0500

    todo list

--- skills-test-react/src/App.js
+++ skills-test-react/src/App.js
@@ -2,29 +2,25 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import AccountMenu from './AccountMenu';
 import BasicForm from './BasicForm'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
-	const [openForm, setOpenForm] = React.useState(false);
-	const handleOnClick = (page) => {
-		console.log('open');
-		if(!openForm) { 
-			setOpenForm(true)
-		} else {
-			setOpenForm(false)
-		}
-	};
+	
   return (
     <Container maxWidth="sm">
-	<AccountMenu onClick={handleOnClick} />
-	{openForm && <Basicorm />}
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          React Skills test
-        </Typography>
-      </Box>
+			<BrowserRouter>
+				<AccountMenu />
+				<Box sx={{ my: 4 }}>
+					<Typography variant="h4" component="h1" gutterBottom>
+						React Skills test
+					</Typography>
+				</Box>
+				<Routes>
+					<Route path='/todo' element={<BasicForm />} />
+				</Routes>
+			</BrowserRouter>
     </Container>
   );
 }
--- skills-test-react/src/BasicForm.js
+++ skills-test-react/src/BasicForm.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from 'react';
 import { Formik, Field, Form } from 'formik';
-import { Button } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-const BasicForm = () => (
+function BasicForm() {
+
+  const [todo, setTodo] = useState();
+
+  const [todoList, setTodoList] = useState([]);
+
+  const SubmitHandler = () => {
+    fetch(`https://62b4742fa36f3a973d3473d0.mockapi.io/api/users`, {
+      method: 'POST',
+      body: JSON.stringify({
+        todo: todo
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+  }
+
+  const getTodos = async() => {
+    const response = await fetch(`https://62b4742fa36f3a973d3473d0.mockapi.io/api/users`)
+    const json = await response.json()
+    setTodoList(json)
+  }
+
+  const deleteHander = (id) => {
+    fetch(`https://62b4742fa36f3a973d3473d0.mockapi.io/api/users/${id}`,
+    {method: 'DELETE'})
+    .then(
+      getTodos()
+    )
+  }
+
+  useEffect(()=> {
+    getTodos()
+    .catch(console.error)
+  },[todoList])
+
+  return(
   <div>
     <h1>TODO</h1>
     <Formik
@@ -15,12 +52,39 @@ const BasicForm = () => (
       onSubmit={(values) => {}}
     >
       <Form>
-        <label htmlFor="todo">Add ToDo </label>
-        <Field id="todo" name="Add ToDo" placeholder="TextHere" />
-        <button type="submit">Submit</button>
+        <div className='todo-section'>
+          <label htmlFor="todo">Add ToDo </label>
+          <Field id="todo" name="Add ToDo" placeholder="TextHere" onChange={e=> setTodo(e.target.value)} />
+          <button type="button" onClick={SubmitHandler}>Submit</button>
+        </div>
       </Form>
     </Formik>
-  </div>
-);
+    
+    <table className='tbl'>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Task</td>
+          <td>Action</td>
+        </tr>
+      </thead>
+      <tbody>
+        {todoList.map((todolist)=> 
+        <tr key={todolist.id}>
+          <td>{todolist.id}</td>
+          <td>{todolist.todo}</td>
+          <td>
+            <button onClick={()=> deleteHander(todolist.id)}>
+              <DeleteForeverIcon />
+            </button>
+            </td>
+        </tr>
+        )}
+      </tbody>
+    </table>
 
-export default BasicForm
+  </div>
+  )
+}
+
+export default BasicForm;
